9fa65d44a4a6b60fd1c8c78e4cfb027d64d53d80
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-09-24 16:36:04 -0300

    Chore: fix typo

--- podcast_blocks/news_summary.py
+++ podcast_blocks/news_summary.py
@@ -11,19 +11,19 @@ class NewsSummary(BasePodcastBlock):
         super(NewsSummary, self).__init__()
 
     def assemble(self) -> Block:
-        chenged_to_in_progress = self.status_changed_count('In Progress')
-        chenged_to_done = self.status_changed_count('Done')
-        chenged_to_selected_for_release = self.status_changed_count(
+        changed_to_in_progress = self.status_changed_count('In Progress')
+        changed_to_done = self.status_changed_count('Done')
+        changed_to_selected_for_release = self.status_changed_count(
             'Selected for Release')
-        chenged_to_live = self.status_changed_count('Live')
+        changed_to_live = self.status_changed_count('Live')
 
         content = f"""
             De ontem para hoje tivemos várias movimentações nos nossos boards.
 
-            {chenged_to_in_progress} tarefas saíram de Ready To Code para em progresso.
-            {chenged_to_done} tarefas foram homologadas.
-            {chenged_to_selected_for_release} tarefas foram selecionadas para produção.
-            E {chenged_to_live} estão em produção.
+            {changed_to_in_progress} tarefas saíram de Ready To Code para em progresso.
+            {changed_to_done} tarefas foram homologadas.
+            {changed_to_selected_for_release} tarefas foram selecionadas para produção.
+            E {changed_to_live} estão em produção.
         """
 
         dialog = Dialog(voice='male', content=content)
--- podcast_blocks/read_to_code_news.py
+++ podcast_blocks/read_to_code_news.py
@@ -10,13 +10,12 @@ class ReadToCodeNews(BasePodcastBlock):
     SOUND_COMMA = 'transition'
 
     def assemble(self) -> Block:
-        issues_chenged_to_ready_to_code = super().get_issues_status_changed_for('Live')
+        issues_changed_to_ready_to_code = super().get_issues_status_changed_for('Live')
 
         list_normalized = list(
             map(lambda issue: f"{issue.summary}, que foi atribuída para {issue.assignee_display_name}",
-                issues_chenged_to_ready_to_code)
+                issues_changed_to_ready_to_code)
         )
-
 
         content = """
           Agora é a hora de falar sobre as tarefas que entraram em Ready To Code de ontem para hoje.
@@ -24,7 +23,8 @@ class ReadToCodeNews(BasePodcastBlock):
           As tarefas são:
         """
 
-        dialog = Dialog(voice='male', content=content + "\n".join(list_normalized))
+        dialog = Dialog(voice='male', content=content +
+                        "\n".join(list_normalized))
 
         self._block.add_dialog(dialog)
 
