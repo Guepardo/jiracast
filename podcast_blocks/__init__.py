2600e7be68f96130ef0d83d04cd053f42cbf1d83
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-09-28 02:06:55 -0300

    Chore: add new block on __init__ package

--- podcast_blocks/__init__.py
+++ podcast_blocks/__init__.py
@@ -1,9 +1,13 @@
 from .greetings import Greetings
 from .read_to_code_news import ReadToCodeNews
+from .in_progress_news import InProgressNews
 from .news_summary import NewsSummary
+from .danger_zone_news import DangerZoneNews
 
 __all__ = [
     'Greetings',
     'NewsSummary',
-    'ReadToCodeNews'
+    'ReadToCodeNews',
+    'InProgressNews',
+    'DangerZoneNews'
 ]
